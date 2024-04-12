cb354f64142cc5af3ab11d356f9a8c1067822c0f
Genrikh Galachyan <galachyan2019@gmail.com>
2024-04-12 22:04:52 +0300

    Добавили код и завершили задание

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string[] input = {"I" , "enjoy" , "learning" , "programming" , "and", "playing" , "games" , "5" , "46", "FD"};
+        string[] shortStrings = FilterShortStrings(input);
+        
+        foreach (var e in shortStrings)
+        {
+            Console.WriteLine(e);
+        }
+    }
+
+    static string[] FilterShortStrings(string[] input)
+    {
+        string[] result = new string[input.Length];
+        int count = 0;
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            if (input[i].Length <= 3)
+            {
+                result[count++] = input[i];
+            }
+        }
+
+        Array.Resize(ref result, count); // Эта строка должна идти ПОСЛЕ цикла, иначе она изменит размер массива в процессе его заполнения
+
+        return result;
+    }
+}
